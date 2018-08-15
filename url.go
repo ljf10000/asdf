4965121b7624576834fd5b894efbba870c896416
ljf10000 <l_j_f@163.com>
2018-08-15 14:59:52 +0800

    .

--- url.go
+++ url.go
@@ -1,5 +1,10 @@
 package asdf
 
+import (
+	"net/url"
+	"strconv"
+)
+
 // param[0] is path
 // param[1] is parameters
 // param[2] is query
@@ -28,4 +33,17 @@ func HttpBaseUrl(host, port, path string) string {
 
 func HttpsBaseUrl(host, port, path string) string {
 	return NewUrl("https", host, port, path)
+}
+
+func UrlParamInt(params url.Values, key string) (int, bool, error) {
+	if values, ok := params[key]; ok {
+		v, err := strconv.Atoi(values[0])
+		if nil != err {
+			Log.Error("url param %s: %d error:%s", v, err)
+		}
+
+		return v, true, err
+	} else {
+		return 0, false, nil
+	}
 }
