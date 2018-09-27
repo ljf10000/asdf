36d55cb3d83ed22211eb0e87d4fb6dfa1a0da5f1
ljf10000 <l_j_f@163.com>
2018-09-27 19:58:26 +0800

    .

---
+++ serialize.go
@@ -0,0 +1,46 @@
+package asdf
+
+import (
+	"bytes"
+	"encoding/gob"
+)
+
+func BinUnmarshal(obj ISlice, data []byte) error {
+	copy(obj.Slice(), data)
+
+	return nil
+}
+
+func BinMarshal(obj ISlice) ([]byte, error) {
+	bin := obj.Slice()
+
+	buf := make([]byte, len(bin))
+	copy(buf, bin)
+
+	return buf, nil
+}
+
+func GobMarshal(obj interface{}) ([]byte, error) {
+	var w bytes.Buffer
+
+	enc := gob.NewEncoder(&w)
+
+	err := enc.Encode(obj)
+	if nil != err {
+		return nil, err
+	}
+
+	return w.Bytes(), nil
+}
+
+func GobUnmarshal(obj interface{}, data []byte) error {
+	r := bytes.NewBuffer(data)
+	dec := gob.NewDecoder(r)
+
+	err := dec.Decode(obj)
+	if nil != err {
+		return err
+	}
+
+	return nil
+}
