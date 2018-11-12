25dbbcc4577ecca0a98a4e1690048c43fd230f80
ljf10000 <l_j_f@163.com>
2018-11-12 15:33:50 +0800

    .

--- ip.go
+++ ip.go
@@ -1,7 +1,6 @@
 package asdf
 
 import (
-	"fmt"
 	"net"
 	"strconv"
 	"strings"
@@ -9,15 +8,30 @@ import (
 )
 
 /******************************************************************************/
+const (
+	IP_ANY IpAddress = 0
+
+	IpAddressSplit = "."
+	IpSubnetSplit  = "/"
+	IpZoneSplit    = "-"
+)
 
 func IpAddressFromString(s string) (IpAddress, error) {
 	ip := [4]byte{}
 
-	n, err := fmt.Sscanf(s, "%d.%d.%d.%d", &ip[0], &ip[1], &ip[2], &ip[3])
+	split := strings.Split(s, IpAddressSplit)
+	n := len(split)
 	if 4 != n {
 		return 0, ErrSprintf("ip address(%s) parse error: n(%d) not 4", s, n)
-	} else if nil != err {
-		return 0, err
+	}
+
+	for i := 0; i < 4; i++ {
+		v, err := strconv.Atoi(split[0])
+		if nil != err {
+			return 0, ErrSprintf("ip address(%s) parse error: %s", s, err)
+		}
+
+		ip[i] = byte(v)
 	}
 
 	return *(*IpAddress)(SlicePointer(ip[:])), nil
@@ -43,30 +57,6 @@ func GetLocalAddress() []string {
 }
 
 /******************************************************************************/
-
-type IpMaskLen byte
-
-func (me IpMaskLen) IsGood() bool {
-	return me > 0 && me <= 32
-}
-
-func (me IpMaskLen) String() string {
-	return strconv.Itoa(int(me))
-}
-
-func (me *IpMaskLen) FromString(s string) error {
-	v, err := strconv.Atoi(s)
-	if nil != err {
-		return err
-	}
-
-	*me = IpMaskLen(v)
-
-	return nil
-}
-
-/******************************************************************************/
-const IP_ANY IpAddress = 0
 
 type IpAddress uint32
 
@@ -98,7 +88,10 @@ func (me IpAddress) String() string {
 func (me IpAddress) ToString() string {
 	ip := MakeSlice(unsafe.Pointer(&me), 4, 4)
 
-	return fmt.Sprintf("%d.%d.%d.%d", ip[0], ip[1], ip[2], ip[3])
+	return strconv.Itoa(int(ip[0])) + IpAddressSplit +
+		strconv.Itoa(int(ip[1])) + IpAddressSplit +
+		strconv.Itoa(int(ip[2])) + IpAddressSplit +
+		strconv.Itoa(int(ip[3]))
 }
 
 func (me *IpAddress) FromString(s string) error {
@@ -116,16 +109,23 @@ func (me IpAddress) MaskEx(length IpMaskLen) IpAddress {
 	return IpAddress((1<<length - 1) << (32 - length))
 }
 
+const (
+	IpMaskA   IpAddress = 0xff000000
+	IpMaskB   IpAddress = 0xffff0000
+	IpMaskC   IpAddress = 0xffffff00
+	IpMaskAll IpAddress = 0xffffffff
+)
+
 func (me IpAddress) Mask(length IpMaskLen) IpAddress {
 	switch length {
 	case 8:
-		return 0xff000000
+		return IpMaskA
 	case 16:
-		return 0xffff0000
+		return IpMaskB
 	case 24:
-		return 0xffffff00
+		return IpMaskC
 	case 32:
-		return 0xffffffff
+		return IpMaskAll
 	default:
 		return me.MaskEx(length)
 	}
@@ -166,7 +166,29 @@ func (me IpAddress) InSubnet(v IpSubnet) bool {
 }
 
 /******************************************************************************/
-const IpSubnetSplit = "/"
+
+type IpMaskLen byte
+
+func (me IpMaskLen) IsGood() bool {
+	return me > 0 && me <= 32
+}
+
+func (me IpMaskLen) String() string {
+	return strconv.Itoa(int(me))
+}
+
+func (me *IpMaskLen) FromString(s string) error {
+	v, err := strconv.Atoi(s)
+	if nil != err {
+		return err
+	}
+
+	*me = IpMaskLen(v)
+
+	return nil
+}
+
+/******************************************************************************/
 
 type IpSubnet struct {
 	Ip  IpAddress `json:"ip"`
@@ -236,8 +258,6 @@ func (me IpSubnet) Include(v IpSubnet) bool {
 }
 
 /******************************************************************************/
-
-const IpZoneSplit = "-"
 
 type IpZone struct {
 	Begin IpAddress `json:"begin"`
@@ -337,442 +357,5 @@ func (me IpZone) Compare(v IpZone) int {
 		//                 |----- v -----|
 		//                                 |----- v -----|
 		return 0
-	}
-}
-
-/******************************************************************************/
-
-const (
-	IpResAddress IpResType = 0
-	IpResSubnet  IpResType = 1
-	IpResZone    IpResType = 2
-	IpResMap     IpResType = 3
-	IpResAny     IpResType = 4
-	IpResEnd     IpResType = 5
-)
-
-type IpResType byte
-
-func (me IpResType) TypeMask() IpResType {
-	return 1 << me
-}
-
-func NewIpFilter() *IpFilter {
-	return &IpFilter{
-		Map: map[IpAddress]bool{},
-	}
-}
-
-type IpFilter struct {
-	TypeMask IpResType
-	Ip       IpAddress
-	Subnet   IpSubnet
-	Zone     IpZone
-	Map      map[IpAddress]bool
-}
-
-func (me *IpFilter) String() string {
-	if me.hasType(IpResAny) {
-		return "any: true"
-	} else {
-		s := Empty
-
-		if me.hasType(IpResAddress) {
-			s += ", ip: " + me.Ip.String()
-		}
-
-		if me.hasType(IpResSubnet) {
-			s += ", subnet: " + me.Subnet.String()
-		}
-
-		if me.hasType(IpResZone) {
-			s += ", zone: " + me.Zone.String()
-		}
-
-		if me.hasType(IpResMap) {
-			ss := Empty
-
-			for k, _ := range me.Map {
-				ss += ", " + k.String()
-			}
-
-			if len(ss) > 0 {
-				s += ", map:[" + ss[2:] + "]"
-			}
-		}
-
-		if len(s) > 0 {
-			s = s[2:]
-		}
-
-		return s
-	}
-}
-
-func (me *IpFilter) hasType(Type IpResType) bool {
-	mask := Type.TypeMask()
-
-	return mask == (mask & me.TypeMask)
-}
-
-func (me *IpFilter) setType(Type IpResType) {
-	me.TypeMask |= Type.TypeMask()
-}
-
-func (me *IpFilter) HasAny() bool {
-	return me.hasType(IpResAny)
-}
-
-func (me *IpFilter) SetAny() {
-	me.setType(IpResAny)
-}
-
-func (me *IpFilter) SetIp(ip IpAddress) {
-	me.setType(IpResAddress)
-	me.Ip = ip
-}
-
-func (me *IpFilter) SetSubnet(subnet IpSubnet) {
-	me.setType(IpResSubnet)
-	me.Subnet = subnet
-}
-
-func (me *IpFilter) SetZone(zone IpZone) {
-	me.setType(IpResZone)
-	me.Zone = zone
-}
-
-func (me *IpFilter) AddIp(ip IpAddress) {
-	me.setType(IpResMap)
-	me.Map[ip] = true
-}
-
-func (me *IpFilter) Match(ip IpAddress) bool {
-	if me.hasType(IpResAny) {
-		return true
-	}
-
-	if me.hasType(IpResAddress) && ip == me.Ip {
-		return true
-	}
-
-	if me.hasType(IpResSubnet) && ip.InSubnet(me.Subnet) {
-		return true
-	}
-
-	if me.hasType(IpResZone) && ip.InZone(me.Zone) {
-		return true
-	}
-
-	if me.hasType(IpResMap) {
-		if _, ok := me.Map[ip]; ok {
-			return true
-		}
-	}
-
-	return false
-}
-
-func (me *IpFilter) Itoa() *IpFilterStr {
-	obj := &IpFilterStr{}
-
-	if me.HasAny() {
-		obj.Any = true
-	} else {
-		if me.hasType(IpResAddress) {
-			obj.Ip = me.Ip.String()
-		}
-
-		if me.hasType(IpResSubnet) {
-			obj.Subnet = me.Subnet.String()
-		}
-
-		if me.hasType(IpResZone) {
-			obj.Zone = me.Zone.String()
-		}
-
-		if me.hasType(IpResMap) {
-			for k, _ := range me.Map {
-				obj.List = append(obj.List, k.String())
-			}
-		}
-	}
-
-	return obj
-}
-
-type IpFilterStr struct {
-	Any    bool     `json:"any"`
-	Ip     string   `json:"ip"`
-	Subnet string   `json:"subnet"`
-	Zone   string   `json:"zone"`
-	List   []string `json:"list"`
-}
-
-func (me *IpFilterStr) String() string {
-	if me.Any {
-		return "any: true"
-	} else {
-		s := Empty
-
-		if Empty != me.Ip {
-			s += ", ip: " + me.Ip
-		}
-
-		if Empty != me.Subnet {
-			s += ", subnet: " + me.Subnet
-		}
-
-		if Empty != me.Zone {
-			s += ", zone: " + me.Zone
-		}
-
-		if len(me.List) > 0 {
-			ss := Empty
-
-			for _, v := range me.List {
-				ss += ", " + v
-			}
-
-			if len(ss) > 0 {
-				s += ", list:[" + ss[2:] + "]"
-			}
-		}
-
-		if len(s) > 0 {
-			s = s[2:]
-		}
-
-		return s
-	}
-}
-
-func (me *IpFilterStr) Atoi() (*IpFilter, error) {
-	obj := NewIpFilter()
-
-	if me.Any {
-		obj.SetAny()
-	} else {
-		if Empty != me.Ip {
-			err := obj.Ip.FromString(me.Ip)
-			if nil != err {
-				return nil, ErrSprintf("parse ip-filter's ip error: %s", err)
-			}
-
-			obj.setType(IpResAddress)
-		}
-
-		if Empty != me.Subnet {
-			err := obj.Subnet.FromString(me.Subnet)
-			if nil != err {
-				return nil, ErrSprintf("parse ip-filter's subnet error: %s", err)
-			}
-
-			obj.setType(IpResSubnet)
-		}
-
-		if Empty != me.Zone {
-			err := obj.Zone.FromString(me.Zone)
-			if nil != err {
-				return nil, ErrSprintf("parse ip-filter's zone error: %s", err)
-			}
-
-			obj.setType(IpResZone)
-		}
-
-		if len(me.List) > 0 {
-			var ip IpAddress
-
-			for k, v := range me.List {
-				err := ip.FromString(v)
-				if nil != err {
-					return nil, ErrSprintf("parse ip-filter's list[%d] error: %s", k, err)
-				}
-
-				obj.Map[ip] = true
-			}
-
-			obj.setType(IpResMap)
-		}
-	}
-
-	return obj, nil
-}
-
-/******************************************************************************/
-
-type IpPairFilter struct {
-	Hit  [2]int
-	Pair [2]IpFilter
-}
-
-func (me *IpPairFilter) String() string {
-	return "[" +
-		strconv.Itoa(me.Hit[0]) + ":" + me.Pair[0].String() + ", " +
-		strconv.Itoa(me.Hit[1]) + ":" + me.Pair[1].String() + "]"
-}
-
-func (me *IpPairFilter) IsMatch(a, b IpAddress) bool {
-	if me.Pair[0].Match(a) && me.Pair[1].Match(b) {
-		me.Hit[0]++
-
-		return true
-	} else if me.Pair[0].Match(b) && me.Pair[1].Match(a) {
-		me.Hit[1]++
-
-		return true
-	} else {
-		return false
-	}
-}
-
-func (me *IpPairFilter) Itoa() *IpPairFilterStr {
-	return &IpPairFilterStr{
-		Pair: [2]IpFilterStr{*me.Pair[0].Itoa(), *me.Pair[1].Itoa()},
-	}
-}
-
-type IpPairFilterStr struct {
-	Pair [2]IpFilterStr `json:"pair"`
-}
-
-func (me *IpPairFilterStr) String() string {
-	return "[" + me.Pair[0].String() + ", " + me.Pair[1].String() + "]"
-}
-
-func (me *IpPairFilterStr) Atoi() (*IpPairFilter, error) {
-	var err error
-	var Pair [2]*IpFilter
-
-	for i := 0; i < 2; i++ {
-		Pair[i], err = me.Pair[i].Atoi()
-		if nil != err {
-			return nil, err
-		}
-	}
-
-	return &IpPairFilter{
-		Pair: [2]IpFilter{*Pair[0], *Pair[1]},
-	}, nil
-}
-
-/******************************************************************************/
-
-type IpPairFilters []*IpPairFilter
-
-func (me IpPairFilters) String() string {
-	s := Empty
-
-	for k, v := range me {
-		s += fmt.Sprintf(", %d:%s", k, v)
-	}
-
-	if len(s) > 0 {
-		s = s[2:]
-	}
-
-	return "[" + s + "]"
-}
-
-func (me IpPairFilters) IsMatch(a, b IpAddress) (*IpPairFilter, bool) {
-	for _, v := range me {
-		if v.IsMatch(a, b) {
-			return v, true
-		}
-	}
-
-	return nil, false
-}
-
-func (me IpPairFilters) Itoa() IpPairFilterStrs {
-	var obj IpPairFilterStrs
-
-	for _, v := range me {
-		obj = append(obj, v.Itoa())
-	}
-
-	return obj
-}
-
-type IpPairFilterStrs []*IpPairFilterStr
-
-func (me IpPairFilterStrs) String() string {
-	s := Empty
-
-	for k, v := range me {
-		s += fmt.Sprintf(", %d:%s", k, v)
-	}
-
-	if len(s) > 0 {
-		s = s[2:]
-	}
-
-	return "[" + s + "]"
-}
-
-func (me IpPairFilterStrs) Atoi() (IpPairFilters, error) {
-	var obj IpPairFilters
-
-	for _, v := range me {
-		tmp, err := v.Atoi()
-		if nil != err {
-			return nil, err
-		}
-
-		obj = append(obj, tmp)
-	}
-
-	return obj, nil
-}
-
-/******************************************************************************/
-func MakeIpPairCache() IpPairCache {
-	return IpPairCache{
-		Cache: map[Ip2Tuple]bool{},
-	}
-}
-
-type IpPairCache struct {
-	Hit   [2]int
-	Cache map[Ip2Tuple]bool
-}
-
-func (me *IpPairCache) String() string {
-	return "[" +
-		strconv.Itoa(me.Hit[0]) + ", " +
-		strconv.Itoa(me.Hit[1]) + "]"
-}
-
-func (me *IpPairCache) isMatch(sip, dip IpAddress) bool {
-	tuple := Ip2Tuple{
-		Sip: sip,
-		Dip: dip,
-	}
-
-	_, ok := me.Cache[tuple]
-
-	return ok
-}
-
-func (me *IpPairCache) Add(sip, dip IpAddress) {
-	tuple := Ip2Tuple{
-		Sip: sip,
-		Dip: dip,
-	}
-
-	me.Cache[tuple] = true
-}
-
-func (me *IpPairCache) IsMatch(sip, dip IpAddress) bool {
-	if me.isMatch(sip, dip) {
-		me.Hit[0]++
-
-		return true
-	} else if me.isMatch(dip, sip) {
-		me.Hit[1]++
-
-		return true
-	} else {
-		return false
 	}
 }
