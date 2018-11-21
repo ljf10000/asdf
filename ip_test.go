b40a57ffdc2f291e16aa3084a245520d6dc47d8f
ljf10000 <l_j_f@163.com>
2018-11-21 16:44:27 +0800

    .

--- ip_test.go
+++ ip_test.go
@@ -17,3 +17,40 @@ func TestIp(t *testing.T) {
 
 	fmt.Printf("ipstring=%s, ip=%x, bin=%v"+Crlf, ipstring, uint32(ip), bin[:])
 }
+
+func TestIpTuple(t *testing.T) {
+	const COUNT = 3000
+
+	tuples := [COUNT * COUNT]Ip2Tuple{}
+	maps := map[Ip2Tuple]uint64{}
+
+	for i := 0; i < COUNT; i++ {
+		for j := 0; j < COUNT; j++ {
+			tuple := Ip2Tuple{
+				Sip: IpAddress(i),
+				Dip: IpAddress(j),
+			}
+
+			tuples[i*COUNT+j] = tuple
+
+			maps[tuple] = 1
+		}
+	}
+
+	hits := uint64(0)
+
+	old := NowTime64()
+
+	for i := 0; i < COUNT*COUNT; i++ {
+		tuple := tuples[i]
+
+		if _, ok := maps[tuple]; ok {
+			hits++
+		}
+	}
+
+	now := NowTime64()
+	ns := uint64(now - old)
+
+	t.Logf("hits: %d, time: %dms, pps: %d\n", hits, ns/1000000, hits*1000000000/ns)
+}
