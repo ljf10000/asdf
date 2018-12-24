ef9b4e63b4e0875c5f7439ccbcd8d34a4ab96c7e
ljf10000 <l_j_f@163.com>
2018-12-24 12:34:34 +0800

    .

--- file_linux.go
+++ file_linux.go
@@ -15,7 +15,7 @@ func (me FileName) Lock() error {
 		return err
 	}
 
-	err = syscall.Flock(f.Fd(), syscall.LOCK_EX|syscall.LOCK_NB)
+	err = syscall.Flock(int(f.Fd()), syscall.LOCK_EX|syscall.LOCK_NB)
 	if nil != err {
 		Log.Error("lock %s error: %s", me, err)
 
