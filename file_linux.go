a475327d9917cd15978cb8e36bc71bf375d83ebf
ljf10000 <l_j_f@163.com>
2018-12-24 15:27:19 +0800

    .

--- file_linux.go
+++ file_linux.go
@@ -8,7 +8,7 @@ import (
 )
 
 func (me FileName) Lock() error {
-	f, err := os.OpenFile(string(me), os.O_RDWR, 0666)
+	f, err := os.OpenFile(string(me), os.O_RDWR|os.O_CREATE, 0666)
 	if nil != err {
 		return err
 	}
