4671a6718ef07b9e401ebb3923c15da231ee1721
ljf10000 <l_j_f@163.com>
2018-12-24 12:37:53 +0800

    .

--- file_linux.go
+++ file_linux.go
@@ -10,15 +10,11 @@ import (
 func (me FileName) Lock() error {
 	f, err := os.OpenFile(string(me), os.O_RDWR, 0666)
 	if nil != err {
-		Log.Error("open %s error: %s", me, err)
-
 		return err
 	}
 
 	err = syscall.Flock(int(f.Fd()), syscall.LOCK_EX|syscall.LOCK_NB)
 	if nil != err {
-		Log.Error("lock %s error: %s", me, err)
-
 		return err
 	}
 
