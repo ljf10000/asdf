6307fada704136e3eaed394d20f08dbdda3c64ed
ljf10000 <l_j_f@163.com>
2018-12-27 17:06:53 +0800

    .

---
+++ zone.go
@@ -0,0 +1,27 @@
+package asdf
+
+type IZoneNode interface {
+	String() string
+	IsGood() bool
+	Zero()
+
+	Up() IZoneNode
+	Down() IZoneNode
+
+	Compare(v IZoneNode) int
+	InRange(a, b IZoneNode) bool
+	InZone(z IZone) bool
+	Add(v IZoneNode) IZoneNode
+}
+
+type IZone interface {
+	NodeBegin() IZoneNode
+	NodeEnd() IZoneNode
+
+	String() string
+	IsGood() bool
+	Zero()
+	Include(v IZone) bool
+	Match(v IZone) bool
+	Intersect(v IZone) IZone
+}
