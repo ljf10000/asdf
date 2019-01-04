20181603e28cd17e37f97e7c3fe6209f0249dfba
ljf10000 <l_j_f@163.com>
2019-01-04 12:13:26 +0800

    .

--- cmd.go
+++ cmd.go
@@ -4,6 +4,11 @@ import (
 	"fmt"
 )
 
+const (
+	cmdPrefixMaybe = '['
+	cmdPrefixMust  = '{'
+)
+
 type CmdOptions []string
 
 func (me CmdOptions) Help() string {
@@ -16,15 +21,27 @@ func (me CmdOptions) Help() string {
 	return s[:len(s)-1]
 }
 
-func (me CmdOptions) match(args []string) bool {
-	count := len(args)
-	if len(me) != count {
-		return false
+func (me CmdOptions) check() (int, error) {
+	count := len(me)
+
+	// only last option support maybe
+	if cmdPrefixMaybe == me[count-1][0] {
+		count--
 	}
 
+	for i := 0; i < count; i++ {
+		if cmdPrefixMaybe == me[i][0] {
+			return 0, ErrBadFormat
+		}
+	}
+
+	return count, nil
+}
+
+func (me CmdOptions) match(args []string) bool {
 	for idx, opt := range me {
 		switch opt[0] {
-		case '{', '[':
+		case cmdPrefixMust, cmdPrefixMaybe: // ignore
 		default:
 			if opt != args[idx] {
 				return false
@@ -36,6 +53,7 @@ func (me CmdOptions) match(args []string) bool {
 }
 
 type Cmd struct {
+	argc    int // ignore [xxx]
 	Options CmdOptions
 	Handler func(args []string) error
 }
@@ -44,8 +62,26 @@ func (me *Cmd) Help() string {
 	return me.Options.Help()
 }
 
+func (me *Cmd) init() error {
+	argc, err := me.Options.check()
+	if nil != err {
+		return err
+	}
+
+	me.argc = argc
+
+	return nil
+}
+
 func (me *Cmd) match(args []string) bool {
-	return me.Options.match(args)
+	argc := len(args)
+	count := len(me.Options)
+
+	if argc == count || argc == me.argc {
+		return me.Options.match(args)
+	} else {
+		return false
+	}
 }
 
 type Command struct {
@@ -73,7 +109,22 @@ func (me *Command) match(args []string) *Cmd {
 	return nil
 }
 
+func (me *Command) init() error {
+	for _, cmd := range me.Cmds {
+		if err := cmd.init(); nil != err {
+			return err
+		}
+	}
+
+	return nil
+}
+
 func (me *Command) Run(args []string) error {
+	err := me.init()
+	if nil != err {
+		return err
+	}
+
 	cmd := me.match(args)
 	if nil != cmd {
 		return cmd.Handler(args)
