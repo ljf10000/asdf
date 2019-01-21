a72fa068c08ae561f3e20bd9f6d40928de2fb45b
ljf10000 <l_j_f@163.com>
2019-01-21 12:22:27 +0800

    .

--- log_console.go
+++ log_console.go
@@ -26,55 +26,55 @@ func (me *ConsoleLogger) Log(level LogLevel, format string, v ...interface{}) {
 
 func (me *ConsoleLogger) Emerg(format string, v ...interface{}) {
 	if LogLevelEmerg <= me.level {
-		me.Log(LogLevelEmerg, format, v...)
+		fmt.Printf(format+Crlf, v...)
 	}
 }
 
 func (me *ConsoleLogger) Alert(format string, v ...interface{}) {
 	if LogLevelAlert <= me.level {
-		me.Log(LogLevelAlert, format, v...)
+		fmt.Printf(format+Crlf, v...)
 	}
 }
 
 func (me *ConsoleLogger) Crit(format string, v ...interface{}) {
 	if LogLevelCrit <= me.level {
-		me.Log(LogLevelCrit, format, v...)
+		fmt.Printf(format+Crlf, v...)
 	}
 }
 
 func (me *ConsoleLogger) Error(format string, v ...interface{}) {
 	if LogLevelError <= me.level {
-		me.Log(LogLevelError, format, v...)
+		fmt.Printf(format+Crlf, v...)
 	}
 }
 
 func (me *ConsoleLogger) Warning(format string, v ...interface{}) {
 	if LogLevelWarning <= me.level {
-		me.Log(LogLevelWarning, format, v...)
+		fmt.Printf(format+Crlf, v...)
 	}
 }
 
 func (me *ConsoleLogger) Warn(format string, v ...interface{}) {
 	if LogLevelWarn <= me.level {
-		me.Log(LogLevelWarn, format, v...)
+		fmt.Printf(format+Crlf, v...)
 	}
 }
 
 func (me *ConsoleLogger) Notice(format string, v ...interface{}) {
 	if LogLevelNotice <= me.level {
-		me.Log(LogLevelNotice, format, v...)
+		fmt.Printf(format+Crlf, v...)
 	}
 }
 
 func (me *ConsoleLogger) Info(format string, v ...interface{}) {
 	if LogLevelInfo <= me.level {
-		me.Log(LogLevelInfo, format, v...)
+		fmt.Printf(format+Crlf, v...)
 	}
 }
 
 func (me *ConsoleLogger) Debug(format string, v ...interface{}) {
 	if LogLevelDebug <= me.level {
-		me.Log(LogLevelDebug, format, v...)
+		fmt.Printf(format+Crlf, v...)
 	}
 }
 
